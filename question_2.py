e2bb6e74fe131063730aba79653b20cccc08c765
Davy Jones <kunal.dekhane@gmail.com>
2024-03-04 13:49:18 -0500

    Updating changes from today 4-3-2024

--- question_2.py
+++ question_2.py
@@ -7,66 +7,88 @@
 # if the user runs out of turns, the program wins saying “You ran out of guesses, the number was: X”.
 
 import random
+def generate_random_number():
 
-# Generating a random number and saving it in a variable
-pick_random_number = random.randint(1, 15)
+    # Generating a random number and saving it in a variable
+    pick_random_number = random.randint(1, 15)
 
-# Prompting user
-print("A random number between 1 and 15 has been picked. You have 3 guesses!")
+    # Prompting user
+    print("A random number between 1 and 15 has been picked. You have 3 guesses!")
 
-#Initializing some variables to tracking user guesses and if they are true
-user_guess = []
-guess_is_correct = False
-i = 0
-exit_game = -1
-#print(pick_random_number)
 
-# Keeping looping till no correct guesses have been entered
-while guess_is_correct == False:
+    return pick_random_number
+    #print(pick_random_number)
 
-# For loop to indicate start of 3 guesses
-    for i in range(0,3):
-        # User's guesses are saved in above initialized list
 
-        user_guess.append(int(input("Please guess the number: ")))
+def get_user_inputs(pick_random_number):
 
-        # Checking to see if user's guess matches with the random number picked earlier
-        # If the guess is correct, indicate correct guess, change guess_is_correct to 'True' to end the while loop
-        # and then break out of the for loop
-        if user_guess[i] == pick_random_number:
-            guess_is_correct = True
-            print("Good guess!")
+    # Initializing some variables for tracking user guesses and if they are true
+    user_guess = []
+    guess_is_correct = False
+    i = 0
+    exit_game = -1
+
+    # Keeping looping till no correct guesses have been entered
+    while guess_is_correct == False:
+
+    # For loop to indicate start of 3 guesses
+        for i in range(0,3):
+            # User's guesses are saved in above initialized list
+
+            while True:
+                try:
+                    user_guess.append(int(input("Please guess the number: ")))
+                    break
+                except:
+                    print("Exception Error: Sorry, input must be an integer number, try again")
+
+
+            # Checking to see if user's guess matches with the random number picked earlier
+            # If the guess is correct, indicate correct guess, change guess_is_correct to 'True' to end the while loop
+            # and then break out of the for loop
+            if user_guess[i] == pick_random_number:
+                guess_is_correct = True
+                print("Good guess!")
+                #print(i)
+                break
+            # Else if the user's entry is '-1' then indicate end of game, change guess_is_correct to 'True' to end the while loop
+            # and then break out of the for loop
+            elif user_guess[i] == exit_game:
+                guess_is_correct = True
+                print("Thanks for playing!")
+                break
+            # Else if the user's guess is lower than the randomly picked number, indicate guess is low
+            elif user_guess[i] < pick_random_number:
+                print("You’re a little low")
+
+            # Else if the user's guess is higher than the randomly picked number, indicate guess is high
+            elif user_guess[i] > pick_random_number:
+                print("You’re a little high")
+
+
+
+        # If 3 guesses have been used up and still no correct number has been guesssed, print out of guesses,
+        # print the randomly generated number, change guess_is_correct to 'True' to end the while loop
+        # and then break out of the while loop.
+        if i > 1 and guess_is_correct == False:
+
+            print("You ran out of guesses, the number was: ", pick_random_number)
             #print(i)
-            break
-        # Else if the user's entry is '-1' then indicate end of game, change guess_is_correct to 'True' to end the while loop
-        # and then break out of the for loop
-        elif user_guess[i] == exit_game:
             guess_is_correct = True
-            print("Thanks for playing!")
             break
-        # Else if the user's guess is lower than the randomly picked number, indicate guess is low
-        elif user_guess[i] < pick_random_number:
-            print("You’re a little low")
 
-        # Else if the user's guess is higher than the randomly picked number, indicate guess is high
-        elif user_guess[i] > pick_random_number:
-            print("You’re a little high")
-
-
-
-    # If 3 guesses have been used up and still no correct number has been guesssed, print out of guesses,
-    # print the randomly generated number, change guess_is_correct to 'True' to end the while loop
-    # and then break out of the while loop.
-    if i > 1 and guess_is_correct == False:
-
-        print("You ran out of guesses, the number was: ", pick_random_number)
-        #print(i)
-        guess_is_correct = True
-        break
+def main():
+    #start of program
+    # rolling of dice after asking user for number of rolls and returning the outcomes of all rolls
+    random_number = generate_random_number()
+    # analyzing the above rolled dice outcomes and output how many times each number was rolled
+    get_user_inputs(random_number)
 
 
-# print(user_guess)
-# print(pick_random_number)
+#main entry point to the program
+if __name__ == "__main__":
+    main()
+
 
 
 
